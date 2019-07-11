a6b96dcb8d1672c132056eda1c80ecddd7233003
xingxichen <xingxichen@users.noreply.github.com>
2019-07-11 17:26:11 +0800

    11

--- hrserver/src/main/java/org/sang/exception/CustomExceptionResolver.java
+++ hrserver/src/main/java/org/sang/exception/CustomExceptionResolver.java
@@ -1,15 +1,15 @@
 package org.sang.exception;
 
-import org.springframework.dao.DataIntegrityViolationException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by sang on 2018/1/2.
